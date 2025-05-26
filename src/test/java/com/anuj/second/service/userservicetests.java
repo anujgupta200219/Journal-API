fcb22662a1139986b6fe1c4f6fbd6d7dcf4c4517
Anuj <anuj.20bce7055@vitap.ac.in>
2025-05-26 21:55:08 +0530

    Commit

--- src/test/java/com/anuj/second/service/userservicetests.java
+++ src/test/java/com/anuj/second/service/userservicetests.java
@@ -1,4 +1,37 @@
 package com.anuj.second.service;
 
+import com.anuj.second.entity.user;
+import com.anuj.second.repository.userrepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 public class userservicetests {
+
+    @Autowired
+    private userrepository repo;
+
+    @Disabled
+    @Test
+    public void testadd(){
+        assertNotNull(repo.findByusername("Ram"));
+        user user = repo.findByusername("Ram");
+        assertTrue(!user.getEntries().isEmpty());
+    }
+
+//    @ParameterizedTest
+//    @CsvSource({
+//            "1,2,3",
+//            "2,1,1",
+//            "4,1,5"
+//    })
+//    public void test(int a,int b,int expected){
+//        assertEquals(expected,a+b,"failed for "+a+" "+b+" "+expected);
+//    }
 }
