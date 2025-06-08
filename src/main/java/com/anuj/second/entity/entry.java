fb1090c7f1ddfa0663ef4a69af249c8a97d5c6ea
Anuj <anuj.20bce7055@vitap.ac.in>
2025-06-08 14:10:54 +0530

    Calling weather API

--- src/main/java/com/anuj/second/entity/entry.java
+++ src/main/java/com/anuj/second/entity/entry.java
@@ -1,5 +1,6 @@
 package com.anuj.second.entity;
 
+import com.anuj.second.enums.sentiment;
 import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -17,4 +18,5 @@ public class entry {
     private String title;
     private String content;
     private LocalDateTime date;
+    private sentiment sentiment;
 }
