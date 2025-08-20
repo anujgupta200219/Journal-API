da8961e7cf906b8729e2be1876c20ee651afd304
Anuj <anuj.20bce7055@vitap.ac.in>
2025-08-20 22:13:27 +0530

    Applying Tokens

--- src/main/java/com/anuj/second/config/SpringSecurity.java
+++ src/main/java/com/anuj/second/config/SpringSecurity.java
@@ -4,16 +4,17 @@ import com.anuj.second.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity{
@@ -36,9 +37,15 @@ public class SpringSecurity{
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(udservice).passwordEncoder(passwordEncoder());
+
     }
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration auth) throws Exception{
+        return auth.getAuthenticationManager();
     }
 }
--- src/main/java/com/anuj/second/controller/publiccontroller.java
+++ src/main/java/com/anuj/second/controller/publiccontroller.java
@@ -4,6 +4,7 @@ import com.anuj.second.entity.user;
 import com.anuj.second.services.UserDetailsServiceImpl;
 import com.anuj.second.services.userservice;
 import com.anuj.second.utils.jwtutil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
-
+@Slf4j
 @RestController
 @RequestMapping("/public")
 public class publiccontroller {
@@ -44,14 +45,17 @@ public class publiccontroller {
     }
 
     @PostMapping("/login")
-    public void login(@RequestBody user d) {
+    public ResponseEntity<?> login(@RequestBody user d) {
         try {
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(d.getUsername(), d.getPassword()));
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(d.getUsername(), d.getPassword()));
             UserDetails userDetails = userDetailsService.loadUserByUsername(d.getUsername());
-
+            String jwt=jwtutil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt,HttpStatus.OK);
         }
         catch(Exception e){
-
+            log.error("**************************Error while generating token*****************************");
+            return new ResponseEntity<>("Error while generating token", HttpStatus.BAD_REQUEST);
         }
     }
 }
