33e651fe9b094ef35842f7ce42c589f715761e43
Anuj <anuj.20bce7055@vitap.ac.in>
2025-08-24 20:33:18 +0530

    Initiating working of Tokens

--- src/main/java/com/anuj/second/config/SpringSecurity.java
+++ src/main/java/com/anuj/second/config/SpringSecurity.java
@@ -1,5 +1,6 @@
 package com.anuj.second.config;
 
+import com.anuj.second.filter.jwtfilter;
 import com.anuj.second.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +15,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 @Configuration
 @EnableWebSecurity
 public class SpringSecurity{
     @Autowired
     private UserDetailsServiceImpl udservice;
+
+    @Autowired
+    private jwtfilter jwtfilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -28,8 +34,8 @@ public class SpringSecurity{
                         .requestMatchers("/controller/**", "/user/**").authenticated()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtfilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
---
+++ src/main/java/com/anuj/second/controller/PublicController.java
@@ -0,0 +1,63 @@
+package com.anuj.second.controller;
+
+import com.anuj.second.entity.user;
+import com.anuj.second.services.UserDetailsServiceImpl;
+import com.anuj.second.services.userservice;
+import com.anuj.second.utils.jwtutil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private jwtutil jwtutil;
+
+    @Autowired
+    private userservice svobj;
+
+    @GetMapping("/Healthcheck")
+    public String Healthcheck() {
+        return "Ok";
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody user d) {
+        if (svobj.save_new_user(d)) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("User already exist", HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody user d) {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(d.getUsername(), d.getPassword())
+            );
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(d.getUsername());
+            String jwt = jwtutil.generateToken(userDetails.getUsername());
+
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error("**************************Error while generating token*****************************", e);
+            return new ResponseEntity<>("Error while generating token", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
